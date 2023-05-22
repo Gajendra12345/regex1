3373b9739a18d9b253422585442ed938aff0bc3c
Gajendra12345 <134274611+Gajendra12345@users.noreply.github.com>
2023-05-22 15:43:07 +0530

    Add files via upload

---
+++ class1.py
@@ -0,0 +1,22 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      in
+#
+# Created:     03-05-2023
+# Copyright:   (c) in 2023
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+class Myclass(object):
+     def show(self):
+        print("I am a Mishra")
+
+x = Myclass()
+x.show()
+
+
+
+
+
+
---
+++ class4.py
@@ -0,0 +1,31 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      in
+#
+# Created:     03-05-2023
+# Copyright:   (c) in 2023
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+class Mobile:
+     def __init__(self):
+        self.model = 'RealMe x'
+
+     def show_model(self):
+        print("model:", self.model)
+
+realme = Mobile()
+
+realme.show_model()
+
+
+
+
+
+
+
+
+
+
+
