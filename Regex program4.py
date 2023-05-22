2d646cc40568a4b06436507341f8c0da19757af5
Gajendra12345 <134274611+Gajendra12345@users.noreply.github.com>
2023-05-22 15:36:10 +0530

    Add files via upload

---
+++ Regex program4.py
@@ -0,0 +1,36 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      in
+#
+# Created:     03-05-2023
+# Copyright:   (c) in 2023
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+import re
+
+
+regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+
+def check(email):
+
+    if(re.search(regex,email)):
+        print("Valid Email")
+    else:
+        print("Invalid Email")
+
+if __name__ == '__main__' :
+
+    email = "rohit.gupta@mcnsolutions.net"
+    check(email)
+
+    email = "praveen@c-sharpcorner.com"
+    check(email)
+
+    email = "inform2atul@gmail.com"
+    check(email)
+
+
+
+
