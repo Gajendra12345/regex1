6377b803b07d1ef0bea4c5c98fc45dceb3308cc9
Gajendra12345 <134274611+Gajendra12345@users.noreply.github.com>
2023-05-22 15:48:54 +0530

    Add files via upload

---
+++ function program with arguments 5.py
@@ -0,0 +1,26 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      in
+#
+# Created:     03-05-2023
+# Copyright:   (c) in 2023
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+# normal function
+def show(x):
+    print(x)
+
+show(5)
+
+# Lambda function
+show = lambda x : print(x)
+show(5)
+
+
+
+
+
+
+
---
+++ function program1.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      in
+#
+# Created:     03-05-2023
+# Copyright:   (c) in 2023
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+def disp():
+    name = "MishraShows"
+    print("Welcome to", name)
+
+disp()
+
+
+
+
